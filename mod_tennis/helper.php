e9be01e88e10a3c5d647a6ed58f499bb3dba9460
Romain Baeriswyl <romain.baeriswyl@alitech.com>
2017-03-08 10:34:26 +0100

    added modal message box

--- mod_tennis/helper.php
+++ mod_tennis/helper.php
@@ -4,6 +4,8 @@ function debug($msg) {
     echo "<script>console.log('".$msg."');</script>";
 }
 
+JHTML::_('behavior.modal', 'a.modal');
+
 class ModTennisHelper
 {
     public static function getWeekReservation($year, $week)
@@ -17,7 +19,7 @@ class ModTennisHelper
         $date->setISODate($year, $week + 1, 1);
         $end = $date->format('Y-m-d');
 
-        $query->select($db->quoteName(array('id', 'user', 'date')))
+        $query->select($db->quoteName(array('id', 'user', 'date', 'partner')))
               ->from($db->quoteName('reservation'))
               ->order($db->quoteName('date') . ' ASC')
               ->where($db->quoteName('date').' >= '.$db->quote($start).' and '.
@@ -28,16 +30,17 @@ class ModTennisHelper
 
         $res_table = array();
         foreach ($res as $r)
-            $res_table[$r->date] = $r->user;
+            $res_table[$r->date] = array("user" => $r->user, "partner" => $r->partner);
 
         return $res_table;
     }
-    
+
     public static function buildCalendar($year, $week)
     {
-        $str = '<input type="submit" class="prev" value="Prev" style="width:50px"/>'.
-            'week '.$week.
-            '<input type="submit" class="next" value="Next" style="width:50px"/>';
+        $str = '<input type="submit" class="weekBtn" value="Prev" id="prev_week"' .
+            ' style="width:50px"/>week ' . $week .
+            '<input type="submit" class="weekBtn" value="Next" id="next_week"' .
+            ' style="width:50px"/>';
 
         $date = new DateTime();
 
@@ -60,7 +63,7 @@ class ModTennisHelper
         $str .= '</tr>';
 
         $table = ModTennisHelper::getWeekReservation($year, $week);
-        
+
         for ($h = $begin; $h <= $end; $h++) {
     
             $str .= '<tr class="row">';
@@ -71,20 +74,39 @@ class ModTennisHelper
                 $date->setTime($h, 0, 0);
                 $d = $date->format('Y-m-d H:i:s');
 
-                $id = $table[$d];
-                if (is_null($id)) {
+                $item = $table[$d];
+
+                if (is_null($item)) {
                     $v = '';
                 } else {
-                    $user = JFactory::getUser($id);
-                    $v = $user->name;
+                    $user = JFactory::getUser($item['user']);
+                    $v = $user->name.'<br>'.$item["partner"];
                 }
+                
                 $str .= '<td class="day" id="cell_'.$i.'_'.$h.'" '.
                     'onclick="reserve_day(\''.$date->format('Y-m-d').'\','.
                     $i.','.$h.')">'.$v.'</td>';
             }
             $str .= '</tr>';
         } 
-        return $str . '</table>';
+        $str .= '</table>';
+
+        $str .= '<style>' .
+            '#message, #partner { top:20%; left:30%;' .
+            'height:100px; width:200px; background-color:lightyellow; ' .
+            'margin:auto; text-align:left; padding:5px; ' .
+            'border:1px solid #ccc; opacity:80; }' .
+            '</style>';
+        
+        $str .= '<div class="modal hide" id="partner">' .
+            '<div class="modal-content"><p>Please select partner type</p>' .
+            '<p><input type="button" class="partner" id="member">Member</input></p>' .
+            '<p><input type="button" class="partner" id="guest">Guest</input></p>'.
+            '</div></div>';
+
+        $str .= '<div class="modal hide" id="message"></div>';
+
+        return $str;
     }
 
     public static function getAjax() {
@@ -99,8 +121,9 @@ class ModTennisHelper
         if (is_null($cmd))
             return "errInval";
 
-        if ($cmd == 'reserve') {
-            # input parameters: date, hour
+        switch ($cmd) {
+        case 'reserve':
+        case 'isUserBusy':
             $user = JFactory::getUser();
             if ($user->guest)
                 return "errGuest";
@@ -112,20 +135,8 @@ class ModTennisHelper
             $date->setTime($input->get('hour'), 0, 0);
             $d = $date->format('Y-m-d H:i:s');
 
-            # check day/hour status
-            $query->select($db->quoteName('user'))
-                  ->from($db->quoteName('reservation'))
-                  ->where($db->quoteName('date') . '=' . $db->quote($d));
-            $db->setQuery($query);
-            $result = $db->loadRow();
-            $query->clear();
-
-            if (is_null($result)) {
-
-                if ($user->authorise('core.admin'))
-                    goto skip_check;
-
-                # check if max number of reservation is reached
+            if ($cmd == 'isUserBusy' and $user->authorise('core.admin') == false) {
+                // check if max number of reservation is reached
                 $query->select($db->quoteName('date'))
                       ->from($db->quoteName('reservation'))
                       ->where($db->quoteName('user') . '=' . $db->quote($user->id));
@@ -135,16 +146,31 @@ class ModTennisHelper
 
                 if ($result->num_rows >= $params->get('max_reserv', 1))
                     return "errMaxReserv";
-
-              skip_check:
-                # add reservation
-                $value = implode(',', array($db->quote($user->id), $db->quote($d)));
+                else
+                    return "";
+            }
+            
+            $partner = $input->get('partner');
+            
+            /* check day/hour status */
+            $query->select($db->quoteName('user'))
+                  ->from($db->quoteName('reservation'))
+                  ->where($db->quoteName('date') . '=' . $db->quote($d));
+            $db->setQuery($query);
+            $result = $db->loadRow();
+            $query->clear();
+            
+            if (is_null($result)) {
+            
+                /* add reservation */
+                $value = implode(',', array($db->quote($user->id), $db->quote($d),
+                	$db->quote($partner)));
 
                 $query->insert($db->quoteName('reservation'))
-                      ->columns($db->quoteName(array('user', 'date')))
+                      ->columns($db->quoteName(array('user', 'date', 'partner')))
                       ->values($value);
 
-                $ret = $user->name;
+                $ret = $user->name.'<br>'.$partner;
                 
             } else {
                 /* rejected if already reserved by another user */
@@ -152,7 +178,7 @@ class ModTennisHelper
                 if ($result[0] != $id)
                     return "errBusy";
                 
-                # remove reservation
+                /* remove reservation */
                 $query->delete($db->quoteName('reservation'))
                       ->where($db->quoteName('user') . '=' . $db->quote($id) .
                       ' and ' . $db->quoteName('date') . '=' . $db->quote($d));
@@ -166,36 +192,41 @@ class ModTennisHelper
             } catch(Exception $e) {
                 $ret = "errInternal";
             }
+
             return $ret;
+            
+      case 'prev_week':
+      case 'next_week':
 
-        } else {
-            # update calendar
-            $session = JFactory::getSession();
+          /* update calendar */
+          $session = JFactory::getSession();
 
-            $year = $session->get('year');
-            $week = $session->get('week');
+          $year = $session->get('year');
+          $week = $session->get('week');
 
-            if ($cmd == 'prev_week') {
-
-                if ($week == 1) {
-                    $week = 52;
-                    $year--;
-                } else
-                    $week--;
+          if ($cmd == 'prev_week') {
+              
+              if ($week == 1) {
+                  $week = 52;
+                  $year--;
+              } else
+                  $week--;
+          }
+          else if ($cmd == 'next_week') {
+              if ($week == 52) {
+                  $week = 1;
+                  $year++;
+              } else
+                  $week++;
+          }
+          
+          $session->set('week', $week);
+          $session->set('year', $year);
+          
+          return ModTennisHelper::buildCalendar($year, $week);
             }
-            else if ($cmd == 'next_week') {
-                if ($week == 52) {
-                    $week = 1;
-                    $year++;
-                } else
-                    $week++;
-            }
-                
-            $session->set('week', $week);
-            $session->set('year', $year);
-
-            return ModTennisHelper::buildCalendar($year, $week);
-        }
+        
+        return $ret;
     }
 }
 
--- mod_tennis/mod_tennis.js
+++ mod_tennis/mod_tennis.js
@@ -1,20 +1,87 @@
 
+/* return true if user is logged in */
+function check_req(cmd)
+{
+    var	req = {
+	'option' : 'com_ajax',
+	'module' : 'tennis',
+	'format' : 'debug',
+	'cmd'    : cmd,
+    }, ret = 0;
+
+    jQuery.ajax({
+	type   : 'POST',
+	data   : req,
+	async  : false, 
+
+	success: function(response) {
+	    console.log(response);
+	    switch (cmd) {
+	    case "isUserBusy":
+		if (response == "errMaxReserv")
+		    ret = 1;
+		break;
+	    }
+	},
+	error: function(response) {
+	    console.log("ajax failed:");
+	    console.log(response);
+	    ret = -1;
+	}
+    })
+    return ret;
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function message(msg)
+{
+    var popup = jQuery("#message");
+    popup.html('<p align="center">' + msg + '</p>' +
+	       '<p align="center"><input type="button" value="close"></input>');
+    popup.modal("show");
+	
+    jQuery('input').click(function() {
+    	popup.modal('hide');
+    });
+}
+
 /* For the current user, if date & hour is free then add reservation
  * else remove it.
  */
-function reserve_day(date, jour, hour)
+function reserve_req(cell, date, hour, partner)
 {
-    var str = 'cell_' + jour + '_' + hour,
-	cell = document.getElementById(str),
-	req = {
-	    'option' : 'com_ajax',
-	    'module' : 'tennis',
-	    'cmd'    : 'reserve',
-	    'date'   : date,
-	    'hour'   : hour,
-	    'format' : 'debug'
-	};
-
+    var	req = {
+	'option' : 'com_ajax',
+	'module' : 'tennis',
+	'format' : 'debug',
+	'cmd'    : 'reserve',
+	'date'   : date,
+	'hour'   : hour,
+	'partner': partner
+    };
+    
     jQuery.ajax({
 	type   : 'POST',
 	data   : req,
@@ -22,19 +89,19 @@ function reserve_day(date, jour, hour)
     	success: function(response) {
 	    switch (response) {
 	    case "errInval":
-		alert("Invalid request");
+		message("Invalid request");
 		break;
 	    case "errGuest":
-		alert("Please login first");
+		message("Please login first");
 		break;
 	    case "errInternal":
-		alert("Server internal error");
+		message("Server internal error");
 		break;
 	    case "errBusy":
-		alert("Already reserved");
+		message("Already reserved");
 		break;
 	    case "errMaxReserv":
-		alert("Max reservation number reached");
+		message("Max reservation number reached");
 		break;
 	    default:
 	    	cell.innerHTML = response;
@@ -42,9 +109,40 @@ function reserve_day(date, jour, hour)
 	},
 	
 	error: function(response) {
+	    console.log("ajax failed:");
 	    console.log(response);
 	}
-    })
+    })   
+}
+
+function reserve_day(date, jour, hour)
+{
+    var str = 'cell_' + jour + '_' + hour,
+	cell = document.getElementById(str),
+	logged = (getCookie("joomla_user_state") == "logged_in");
+
+    if (logged == false) {
+	message("Please log in first");
+    	return;
+    }
+    
+    if (cell.innerHTML == '') {
+
+	if (check_req('isUserBusy')) {
+	    message("You'reached max number of reservation");
+	    return;
+	}
+
+	var popup = jQuery("#partner");
+	popup.modal('show');
+
+	jQuery('.partner').click(function(event) {
+	    popup.modal('hide');
+	    reserve_req(cell, date, hour, event.target.id);
+	});
+	
+    } else
+	reserve_req(cell, date, hour, null);
 }
 
 function change_week(cmd)
@@ -67,7 +165,8 @@ function change_week(cmd)
 	},
 	
 	error: function(response) {
-	    console.log("change_week error " + response);
+	    console.log("change_week error");
+	    console.log(response);
 	}
     })
 }
@@ -75,16 +174,13 @@ function change_week(cmd)
 function addEvent() {
     jQuery(".day").hover(
 	function() {
-	    jQuery(this).css("background-color", "lightblue");
+	    jQuery(this).css("background-color", "lightyellow");
 	},
 	function() {
 	    jQuery(this).css("background-color", "white");
 	})
-    jQuery(".prev").click(function() {
-	change_week("prev_week");
-    })
-    jQuery(".next").click(function() {
-	change_week("next_week");
+    jQuery(".weekBtn").click(function(event) {
+	change_week(event.target.id);
     })
 }
 
