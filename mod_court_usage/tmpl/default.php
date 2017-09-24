79682eb49294691c03a40ea1f0423f5b6a329ef0
Romain Baeriswyl <romain.baeriswyl@alitech.com>
2017-09-24 10:01:18 +0200

    update

--- mod_court_usage/tmpl/default.php
+++ mod_court_usage/tmpl/default.php
@@ -4,8 +4,8 @@ defined('_JEXEC') or die;
 echo '<div id="chart1"></div><hr>'.
     '<div id="chart2"></div><hr>'.
     '<p>Exporter les reservations faites du '.
-    '<input type="text" name="debut" class="dp" id="exportBegin" value="2017-01-01" style="width:80px"/>'.
-    ' au <input type="text" name="fin" class="dp" id="exportEnd" value="2017-12-31" style="width:80px"/>.</p>'.
+    '<input type="text" name="debut" class="dp" id="exportBegin" value="2017-01-01" style="width:100px"/>'.
+    ' au <input type="text" name="fin" class="dp" id="exportEnd" value="2017-12-31" style="width:100px"/>.</p>'.
     '<input type="submit" class="exportBtn" value="export" id="exportDb"/>';
 
 ?>
