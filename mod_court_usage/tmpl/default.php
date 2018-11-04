2d6f215c3fa109f9871c732f7d7539d05191f045
TC Le Vaud <tclvuser@fistfight.dreamhost.com>
2018-11-04 11:36:51 -0800

    added charts

--- mod_court_usage/tmpl/default.php
+++ mod_court_usage/tmpl/default.php
@@ -4,6 +4,8 @@ defined('_JEXEC') or die;
 echo '<div id="status"></div><hr>'.
     '<div id="chart1"></div><hr>'.
     '<div id="chart2"></div><hr>'.
+    '<div id="chart3"></div><hr>'.
+    '<div id="chart4"></div><hr>'.
     '<div id="excel"></div>';
 
 ?>
